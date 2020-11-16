df86d5e511fe7c5fbf4d71ae6255ee538b88e8f5
Bernard Teo <btzy1996@hotmail.com>
2020-11-17 01:00:45 +0800

    Add sum test

---
+++ src/test/sum.j
@@ -0,0 +1,16 @@
+class Main {
+
+Void main(){
+  Int n;
+  Int i;Int sum;
+  n=10;
+  println(n);
+  i=1;
+  sum = 0;
+  while (i <= n){
+    sum = sum+i;
+    i=i+1;
+  }
+  println(sum);
+}
+}
---
+++ src/test/sum.s
@@ -0,0 +1,45 @@
+.text
+.global main
+main:
+stmfd sp!,{a1,fp,lr}
+sub sp,sp,#12
+mov fp,#10
+str fp,[sp,#8]
+ldr a2,[sp,#8]
+ldr a1,=.LZ0
+bl printf(PLT)
+mov fp,#1
+str fp,[sp,#4]
+mov fp,#0
+str fp,[sp,#0]
+.L0:
+ldr fp,[sp,#4]
+ldr lr,[sp,#8]
+cmp fp,lr
+movle fp,#1
+movgt fp,#0
+cmp fp,#0
+bne .L1
+b .L2
+.L1:
+ldr fp,[sp,#0]
+ldr lr,[sp,#4]
+add fp,fp,lr
+str fp,[sp,#0]
+ldr fp,[sp,#4]
+mov lr,#1
+add fp,fp,lr
+str fp,[sp,#4]
+b .L0
+.L2:
+ldr a2,[sp,#0]
+ldr a1,=.LZ0
+bl printf(PLT)
+mov a1,#0
+add sp,sp,#16
+ldmfd sp!,{fp,pc}
+add sp,sp,#16
+ldmfd sp!,{fp,pc}
+.data
+.LZ0:
+.asciz "%d\012\000"
