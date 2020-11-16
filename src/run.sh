e5647189f561fad4a3b79700e3060448b2478523
Bernard Teo <btzy1996@hotmail.com>
2020-11-16 21:42:12 +0800

    Fix run script

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -cp cup/java-cup-11b-runtime.jar:. Compiler -i test/$1.j > runner_file.s
 arm-linux-gnueabi-gcc -march=armv7-a runner_file.s --static -o runner_file
-$(HOME)/Documents/gem5/build/ARM/gem5.opt gem5/configs/example/se.py -c ./runner_file
+$HOME/Documents/gem5/build/ARM/gem5.opt $HOME/Documents/gem5/configs/example/se.py -c ./runner_file
 rm runner_file.s
 rm runner_file
