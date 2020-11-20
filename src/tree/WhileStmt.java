0b11cef4e6cbeb08268789458f6f3e0ec51d2501
Bernard Teo <btzy1996@hotmail.com>
2020-11-21 01:38:47 +0800

    Fix WhileStmt IR3 bug

--- src/tree/WhileStmt.java
+++ src/tree/WhileStmt.java
@@ -30,6 +30,12 @@ public class WhileStmt extends Stmt {
 	 * Typecheck and emit IR3 code for this node.
 	 */
 	public void typeCheckAndEmitIR3(Context ctx, Consumer<? super ir3.Instruction> out) throws SemanticException {
+		ir3.Label start_label = ctx.newLabel();
+		ir3.Label in_label = ctx.newLabel();
+		ir3.Label end_label = ctx.newLabel();
+
+		out.accept(start_label);
+
 		// will throw if the type can't be converted (the only time it might be converted is for nulls)
 		// condition must be Bool
 		ir3.Expr cond_res = SemanticException.bound(() -> {
@@ -39,13 +45,8 @@ public class WhileStmt extends Stmt {
 				.orElseThrow(() -> new ir3.TypeImbueWhileStatementConditionException(cond_nullable.getType(), cond.range))
 				.makeRelExp3ByMaybeEmitIR3(cond.range, ctx, out);
 		}, new ir3.BooleanLiteral(true));
-		
-		ir3.Label start_label = ctx.newLabel();
-		ir3.Label in_label = ctx.newLabel();
-		ir3.Label end_label = ctx.newLabel();
 
 		// generate the IR3 while loop code
-		out.accept(start_label);
 		out.accept(new ir3.IfGoto(cond_res, in_label));
 		out.accept(new ir3.Goto(end_label));
 		out.accept(in_label);
